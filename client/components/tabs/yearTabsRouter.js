2f10638e4e7e645bb6185b08ce878845eac90195
jasonc1025 <jasonc@eande.world>
2018-04-01 19:49:31 -0700

    Add '[..]' to create space between years.  Import 'Glyphicon'.

--- client/components/tabs/yearTabsRouter.js
+++ client/components/tabs/yearTabsRouter.js
@@ -1,7 +1,7 @@
 //client/components/tabs/yearTabsRouter.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Tab, Tabs } from 'react-bootstrap'
+import { Tab, Tabs, Glyphicon } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 class YearTabsRouter extends React.Component {
 constructor(){
@@ -10,8 +10,8 @@ constructor(){
  }
  render(){
    return <Link to={{pathname: '/', search: '?month=All&year='+this.props.year }} >
-     <p style={this.state.style}>{this.props.year}</p>
-    </Link>
+     <p style={this.state.style}>[ {this.props.year} ]</p>
+     </Link>
  }
 }
 export default YearTabsRouter;
